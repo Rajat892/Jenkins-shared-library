c671ab45156464b213a653cd481c922be92280e6
Rajat892 <102934281+Rajat892@users.noreply.github.com>
2025-05-31 01:28:07 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,1 +1,12 @@
-
+def call(String username, String userpassword, String image, String tag){
+  withCredentials(
+    [
+      usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')
+    ]
+  )
+  {
+     sh "docker login -u ${username} -p ${userpassword}"
+  }
+  sh "docker push ${username}/${image}:${tag}"
+  echo "Image pushed to DockerHub"
+}
