f9990132b8b9a1bdaf7a7f1e0db76021dc55fa97
Rajat892 <102934281+Rajat892@users.noreply.github.com>
2025-06-09 23:39:41 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,5 +1,4 @@
 def call(){
-  sh "cd /home/ubuntu/workspace/Wanderlust-CI/frontend && sudo npm install && cd ../backend && sudo npm install && cd .. && sudo npm install"
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
